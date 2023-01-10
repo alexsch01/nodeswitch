d326a4522adf0e8d13df4f1e85a2331f4034cc23
Alex Schwartz <alexschwartz01@gmail.com>
2023-01-10 16:37:45 -0500

    SUPPORT Git Bash [part3]

--- scripts/postinstall.cmd
+++ scripts/postinstall.cmd
@@ -1,3 +1,7 @@
+copy .\bin\nodeswitch %AppData%\npm
 copy .\bin\nodeswitch.cmd %AppData%\npm
 copy .\bin\nodeswitch.ps1 %AppData%\npm
+copy .\bin\nodeswitch.sh %AppData%\npm
+findstr /m "alias nodeswitch=" %userprofile%\.bash_profile > nul
+if %errorlevel% == 1 ( echo alias nodeswitch="source nodeswitch" >> %userprofile%\.bash_profile )
 if not exist %AppData%\nodeswitch ( mkdir %AppData%\nodeswitch )
