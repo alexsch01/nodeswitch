15e6995c7b5a9b99410600e582dcb3c134d8c20b
Alex Schwartz <alexschwartz01@gmail.com>
2023-10-24 10:08:51 -0400

    make structure of bash script like the others

--- bin/nodeswitch.sh
+++ bin/nodeswitch.sh
@@ -1,62 +1,69 @@
-if [ "$#" = "2" ]
+if [ "$1" != "" ]
 then
-    if [ "$1" = "remove" ]
+    if [ "$2" != "" ]
     then
-        if [ -d "$APPDATA/nodeswitch/$2" ]
+        if [ "$3" != "" ]
         then
-            rm -rf $APPDATA/nodeswitch/$2
+            echo "Incorrect command"
         else
-            echo "Node version not installed"
-        fi
-    elif [ "$1" = "use" ]
-    then
-        if [ "$2" = "default" ]
-        then
-            if [ ! -z "$nodeswitchDefaultPATH" ]
+            if [ "$1" = "remove" ]
             then
-                export PATH=$nodeswitchDefaultPATH
-            fi
-        else
-            if [ -d "$APPDATA/nodeswitch/$2" ]
-            then
-                if [ -z "$nodeswitchDefaultPATH" ]
+                if [ -d "$APPDATA/nodeswitch/$2" ]
                 then
-		    export nodeswitchDefaultPATH=$PATH
-                    export PATH=$APPDATA/nodeswitch/$2:$PATH
+                    rm -rf $APPDATA/nodeswitch/$2
                 else
-		    export PATH=$APPDATA/nodeswitch/$2:$nodeswitchDefaultPATH
+                    echo "Node version not installed"
+                fi
+            elif [ "$1" = "use" ]
+            then
+                if [ "$2" = "default" ]
+                then
+                    if [ ! -z "$nodeswitchDefaultPATH" ]
+                    then
+                        export PATH=$nodeswitchDefaultPATH
+                    fi
+                else
+                    if [ -d "$APPDATA/nodeswitch/$2" ]
+                    then
+                        if [ -z "$nodeswitchDefaultPATH" ]
+                        then
+                            export nodeswitchDefaultPATH=$PATH
+                            export PATH=$APPDATA/nodeswitch/$2:$PATH
+                        else
+                            export PATH=$APPDATA/nodeswitch/$2:$nodeswitchDefaultPATH
+                        fi
+                    else
+                        echo "Node version not installed"
+                    fi
+                fi
+            elif [ "$1" = "add" ]
+            then
+                if [ ! -d "$APPDATA/nodeswitch/$2" ]
+                then
+                    curl -f https://nodejs.org/download/release/v$2 &>/dev/null
+                    if [ $? -eq 0 ]
+                    then
+                        curl -s -o $APPDATA/nodeswitch/$2.zip https://nodejs.org/download/release/v$2/node-v$2-win-x64.zip > /dev/null
+                        unzip -d $APPDATA/nodeswitch -o $APPDATA/nodeswitch/$2.zip > /dev/null
+                        rm $APPDATA/nodeswitch/$2.zip
+                        mv $APPDATA/nodeswitch/node-v$2-win-x64 $APPDATA/nodeswitch/$2 > /dev/null
+                    else
+                        echo "Node version not found"
+                    fi
+                else
+                    echo "Node version already added"
                 fi
             else
-                echo "Node version not installed"
+                echo "Incorrect command"
             fi
         fi
-    elif [ "$1" = "add" ]
-    then
-        if [ ! -d "$APPDATA/nodeswitch/$2" ]
+    else
+        if [ "$1" = "list" ]
         then
-            curl -f https://nodejs.org/download/release/v$2 &>/dev/null
-            if [ $? -eq 0 ]
-            then
-                curl -s -o $APPDATA/nodeswitch/$2.zip https://nodejs.org/download/release/v$2/node-v$2-win-x64.zip > /dev/null
-                unzip -d $APPDATA/nodeswitch -o $APPDATA/nodeswitch/$2.zip > /dev/null
-                rm $APPDATA/nodeswitch/$2.zip
-                mv $APPDATA/nodeswitch/node-v$2-win-x64 $APPDATA/nodeswitch/$2 > /dev/null
-            else
-                echo "Node version not found"
-            fi
+            dir -1 $APPDATA/nodeswitch
         else
-            echo "Node version already added"
+            echo "Incorrect command"
         fi
-    else
-        echo "Incorrect command"
-    fi
-elif [ "$#" = "1" ]
-then
-    if [ "$1" = "list" ]
-    then
-        dir -1 $APPDATA/nodeswitch
-    else
-        echo "Incorrect command"
     fi
 else
     echo "Incorrect command"
