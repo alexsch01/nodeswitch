eab2fa1bf7b0ba44f96d3c4a32f911d52594f6db
Alex Schwartz <alexschwartz01@gmail.com>
2023-10-24 17:28:50 -0400

    Update preinstall.js

--- scripts/preinstall.js
+++ scripts/preinstall.js
@@ -1,3 +1,8 @@
+if(!process.env.npm_config_global) {
+    console.log('This npm module can only be installed globally')
+    process.exit(1)
+}
+
 if(process.platform != 'win32' || process.arch != 'x64') {
     console.log('This npm module is only for Microsoft Windows x64')
     process.exit(1)
