32997e3797ed550b0629a0609c3a4485e334c2e3
Alex Schwartz <alexschwartz01@gmail.com>
2024-10-29 10:22:52 -0400

    Update nodeswitch.ps1

--- bin/nodeswitch.ps1
+++ bin/nodeswitch.ps1
@@ -67,6 +67,8 @@ if ( $nodeswitch1stParameter -ne $null ) {
             dir -n $env:AppData\nodeswitch 
         } elseif ( $nodeswitch1stParameter -eq "path" ) {
             echo "$env:AppData\nodeswitch"
+        } elseif ( $nodeswitch1stParameter -eq "pathopen" ) {
+            explorer $(nodeswitch path)
         } else {
             echo "Incorrect command"
         }
