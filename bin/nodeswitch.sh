b0de2aed7d1fb168e2fcceca602afa037bdc2c92
Alex Schwartz <alexschwartz01@gmail.com>
2025-08-21 08:41:16 -0400

    [sh] convert to unix path

--- bin/nodeswitch.sh
+++ bin/nodeswitch.sh
@@ -1,3 +1,5 @@
+nodeswitchAPPDATA=$(cygpath -u $nodeswitchAPPDATA)
+
 if [ "$1" != "" ]
 then
     if [ "$2" != "" ]
@@ -8,9 +10,9 @@ then
         else
             if [ "$1" = "remove" ]
             then
-                if [ -d "$APPDATA/nodeswitch/$2" ]
+                if [ -d "$nodeswitchAPPDATA/nodeswitch/$2" ]
                 then
-                    rm -rf $APPDATA/nodeswitch/$2
+                    rm -rf $nodeswitchAPPDATA/nodeswitch/$2
                 else
                     echo "Node version not installed"
                 fi
@@ -23,14 +25,14 @@ then
                         export PATH=$nodeswitchDefaultPATH
                     fi
                 else
-                    if [ -d "$APPDATA/nodeswitch/$2" ]
+                    if [ -d "$nodeswitchAPPDATA/nodeswitch/$2" ]
                     then
                         if [ -z "$nodeswitchDefaultPATH" ]
                         then
                             export nodeswitchDefaultPATH=$PATH
-                            export PATH=$APPDATA/nodeswitch/$2:$PATH
+                            export PATH=$nodeswitchAPPDATA/nodeswitch/$2:$PATH
                         else
-                            export PATH=$APPDATA/nodeswitch/$2:$nodeswitchDefaultPATH
+                            export PATH=$nodeswitchAPPDATA/nodeswitch/$2:$nodeswitchDefaultPATH
                         fi
                     else
                         echo "Node version not installed"
@@ -38,32 +40,32 @@ then
                 fi
             elif [ "$1" = "add" ]
             then
-                if [ ! -d "$APPDATA/nodeswitch/$2" ]
+                if [ ! -d "$nodeswitchAPPDATA/nodeswitch/$2" ]
                 then
                     curl -f https://nodejs.org/download/release/v$2/ &>/dev/null
                     if [ $? -eq 0 ]
                     then
-                        curl -s -o $APPDATA/nodeswitch/$2.zip https://nodejs.org/download/release/v$2/node-v$2-win-x64.zip > /dev/null
+                        curl -s -o $nodeswitchAPPDATA/nodeswitch/$2.zip https://nodejs.org/download/release/v$2/node-v$2-win-x64.zip > /dev/null
 
                         if [ $? -ne 0 ]
                         then
-                            rm $APPDATA/nodeswitch/$2.zip
+                            rm $nodeswitchAPPDATA/nodeswitch/$2.zip
                             echo "Node version not created"
                             exit
                         fi
 
-                        unzip -d $APPDATA/nodeswitch -o $APPDATA/nodeswitch/$2.zip > /dev/null
+                        unzip -d $nodeswitchAPPDATA/nodeswitch -o $nodeswitchAPPDATA/nodeswitch/$2.zip > /dev/null
 
                         if [ $? -ne 0 ]
                         then
-                            rm $APPDATA/nodeswitch/$2.zip
-                            rm -rf $APPDATA/nodeswitch/node-v$2-win-x64
+                            rm $nodeswitchAPPDATA/nodeswitch/$2.zip
+                            rm -rf $nodeswitchAPPDATA/nodeswitch/node-v$2-win-x64
                             echo "Node version not created"
                             exit
                         fi
 
-                        rm $APPDATA/nodeswitch/$2.zip
-                        mv $APPDATA/nodeswitch/node-v$2-win-x64 $APPDATA/nodeswitch/$2 > /dev/null
+                        rm $nodeswitchAPPDATA/nodeswitch/$2.zip
+                        mv $nodeswitchAPPDATA/nodeswitch/node-v$2-win-x64 $nodeswitchAPPDATA/nodeswitch/$2 > /dev/null
                     else
                         echo "Node version not found"
                     fi
@@ -77,10 +79,10 @@ then
     else
         if [ "$1" = "list" ]
         then
-            dir -1 $APPDATA/nodeswitch
+            dir -1 $nodeswitchAPPDATA/nodeswitch
         elif [ "$1" = "path" ]
         then
-            echo "$APPDATA\nodeswitch"
+            echo "$nodeswitchAPPDATA\nodeswitch"
         elif [ "$1" = "pathopen" ]
         then
             start explorer $(nodeswitch path)
